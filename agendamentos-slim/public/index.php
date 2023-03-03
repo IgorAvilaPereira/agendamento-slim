2a82c17a9ebe294c4db992e75b9fd108db0c0d07
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-03-03 20:03:54 -0300

    first commit

--- agendamentos-slim/public/index.php
+++ agendamentos-slim/public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -19,46 +20,44 @@ $app->get('/listar_usuarios', function (Request $request, Response $response, $a
     $mysqli = new mysqli("localhost", "root", "root", "agendamento_users");
     $result = $mysqli->query("SELECT * FROM cms_usuario LIMIT 10");
     // printf("Select returned %d rows.\n", $result->num_rows);
-    while ($row = $result->fetch_array(MYSQLI_ASSOC)){
-        $response->getBody()->write($row["id_usuario"].",".$row["nome"]."<br>");
+    while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+        $response->getBody()->write($row["id_usuario"] . "," . $row["nome"] . "<br>");
     }
     return $response;
 });
 /*
 
-$app->get('/listar_agendamentos', function (Request $request, Response $response, $args) {
-    $mysqli = new mysqli("localhost", "root", "root", "agendamento");
-    $result = $mysqli->query("SELECT * FROM agendamentos LIMIT 10");
-    // printf("Select returned %d rows.\n", $result->num_rows);
-    while ($row = $result->fetch_array(MYSQLI_ASSOC)){
-        $response->getBody()->write($row["id"].",".$row["data"]."<br>");
-    }
-    return $response;   
-    
-});*/
+  $app->get('/listar_agendamentos', function (Request $request, Response $response, $args) {
+  $mysqli = new mysqli("localhost", "root", "root", "agendamento");
+  $result = $mysqli->query("SELECT * FROM agendamentos LIMIT 10");
+  // printf("Select returned %d rows.\n", $result->num_rows);
+  while ($row = $result->fetch_array(MYSQLI_ASSOC)){
+  $response->getBody()->write($row["id"].",".$row["data"]."<br>");
+  }
+  return $response;
+
+  }); */
 
 $app->get('/listar_agendamentos', function (Request $request, Response $response, $args) {
     $mysqli = new mysqli("localhost", "root", "root", "agendamento");
     $result = $mysqli->query("SELECT * FROM agendamentos LIMIT 10");
     // printf("Select returned %d rows.\n", $result->num_rows);
     $vetAgendamento = $result->fetch_all(MYSQLI_ASSOC);
-    /*while ($row = $result->fetch_array(MYSQLI_ASSOC)){
-        $response->getBody()->write($row["id"].",".$row["data"]."<br>");
-    }*/
+    /* while ($row = $result->fetch_array(MYSQLI_ASSOC)){
+      $response->getBody()->write($row["id"].",".$row["data"]."<br>");
+      } */
     $view = Twig::fromRequest($request);
     return $view->render($response, 'agendamentos/listar.html', [
         'vetAgendamento' => $vetAgendamento,
     ]);
 })->setName('listar_agendamentos');
 
-
-
 $app->get('/listar_laboratorios', function (Request $request, Response $response, $args) {
     $mysqli = new mysqli("localhost", "root", "root", "agendamento");
     $result = $mysqli->query("SELECT * FROM laboratorios LIMIT 10");
     // printf("Select returned %d rows.\n", $result->num_rows);
-    while ($row = $result->fetch_array(MYSQLI_ASSOC)){
-        $response->getBody()->write($row["id"].",".$row["nome"]."<br>");
+    while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+        $response->getBody()->write($row["id"] . "," . $row["nome"] . "<br>");
     }
     return $response;
 });
@@ -67,17 +66,17 @@ $app->get('/listar_horarios', function (Request $request, Response $response, $a
     $mysqli = new mysqli("localhost", "root", "root", "agendamento");
     $result = $mysqli->query("SELECT * FROM horarios LIMIT 10");
     // printf("Select returned %d rows.\n", $result->num_rows);
-    while ($row = $result->fetch_array(MYSQLI_ASSOC)){
-        $response->getBody()->write($row["id"].",".$row["hora_inicio"].",".$row["hora_fim"]."<br>");
+    while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+        $response->getBody()->write($row["id"] . "," . $row["hora_inicio"] . "," . $row["hora_fim"] . "<br>");
     }
     return $response;
 });
 
 $app->get('/tela_adicionar_agendamento', function (Request $request, Response $response, $args) {
     $view = Twig::fromRequest($request);
-    return $view->render($response, 'agendamentos/tela_adicionar.html'/*, [
-        'name' => $args['name'],
-    ]*/);
+    return $view->render($response, 'agendamentos/tela_adicionar.html'/* , [
+              'name' => $args['name'],
+              ] */);
 })->setName('tela_adicionar_agendamento');
 
 $app->post('/adicionar_agendamento', function (Request $request, Response $response, $args) {
@@ -85,12 +84,11 @@ $app->post('/adicionar_agendamento', function (Request $request, Response $respo
     return $response;
 });
 
-
 $app->get('/tela_editar_agendamento', function (Request $request, Response $response, $args) {
     $view = Twig::fromRequest($request);
-    return $view->render($response, 'tela_editar.html'/*, [
-        'name' => $args['name'],
-    ]*/);
+    return $view->render($response, 'tela_editar.html'/* , [
+              'name' => $args['name'],
+              ] */);
 })->setName('tela_editar_agendamento');
 
 $app->post('/editar_agendamento/{id}', function (Request $request, Response $response, $args) {
@@ -127,18 +125,18 @@ $app->get('/delete_lab/{id}', function (Request $request, Response $response, $a
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $view = Twig::fromRequest($request);
-    return $view->render($response, 'index.html'/*, [
-        'name' => $args['name'],
-    ]*/);
+    return $view->render($response, 'index.html'/* , [
+              'name' => $args['name'],
+              ] */);
 })->setName('index');
 
 /*
-//  ex. templates
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    $view = Twig::fromRequest($request);
-    return $view->render($response, 'profile.html', [
-        'name' => $args['name'],
-    ]);
-})->setName('profile');*/
+  //  ex. templates
+  $app->get('/hello/{name}', function ($request, $response, $args) {
+  $view = Twig::fromRequest($request);
+  return $view->render($response, 'profile.html', [
+  'name' => $args['name'],
+  ]);
+  })->setName('profile'); */
 
 $app->run();
